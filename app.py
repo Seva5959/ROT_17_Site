83fb4adbba4216de69bf0ef82258bd987dcc0451
seva5959 <vsevolodgannik@gmail.com>
2025-05-04 22:44:33 +0500

    Именил арабские цифры на римские

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def index():
     # Обновляем статус решения для каждого кода
     for code in codes:
         code.solved = solved_dict.get(code.id, False)
+        code.roman_number = to_roman(code.number)
 
     return render_template('index.html', codes=codes, answers=CORRECT_ANSWERS)
 
@@ -196,3 +197,23 @@ def make_admin(user_id):
     flash(f'Пользователь {user.username} теперь администратор')
     return redirect(url_for('admin_dashboard'))
 
+
+def to_roman(num):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4, 1
+    ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    ]
+    roman_num = ''
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+        i += 1
+    return roman_num
